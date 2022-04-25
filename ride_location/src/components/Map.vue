76832bd29bab769e074d47c4413bfec5af7f2c7c
alejjuuu <lav29@njit.edu>
2022-04-24 21:15:37 -0400

    click on the map markers, begining of destination on the map, autocompletion

--- ride_location/src/components/Map.vue
+++ ride_location/src/components/Map.vue
@@ -1,5 +1,6 @@
 <template>
   <div id='container'>
+    <!---
     <div class="row">
       <div class="col-md-6">
           <h1>Your coordinates:</h1>
@@ -10,25 +11,80 @@
         <p>{{ mapCoordinates.lat }} Latitude, {{ mapCoordinates.lng }} Longitude</p>
       </div>
     </div>
-      <br>
+    --->
     <div class="row">
-    <div class="col-md-6">
-        <div class="column">
-          <form clas="ui segment large form">
-            <div class="ui message red"></div>
-            <div class="ui segment">
-              <div class="field">
-                <div class="ui right icon input large">
-                  <h3>Street name</h3>
-                  <input id="autocomplete" placeholder="autofilled on" type="text"/>
-                </div>
-              </div>
-              <button class="ui button">GO</button>
-            </div>
-          </form>
-        </div>
+      <div class="col-md-12">
+        <form id="distance_form">
+          <div class="form-group">
+              <label>Origin: </label>
+              <input class="form-control" id="from_places" placeholder="Enter a location" />
+              <input id="origin" name="origin" required="" type="hidden" />
+          </div>
+
+          <div class="form-group">
+              <label>Destination: </label>
+              <input class="form-control" id="to_places" placeholder="Enter a location" />
+              <input id="destination" name="destination" required="" type="hidden" />
+          </div>
+          <br>
+          <input class="btn btn-primary" type="submit" value="Calculate" />
+        </form>
+        <br>
       </div>
-      <div class="col-md-6">
+      <div id="result">
+          <ul class="list-group">
+              <li id="mile" class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-center
+               ">
+                Distance In Mile :
+              </li>
+              <li id="kilo" class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-center
+                ">
+                Distance is Kilograms:
+              </li>
+                <li id="text" class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-center
+                ">
+                IN TEXT:
+              </li>
+                <li id="minute" class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-center
+                ">
+                IN MINUTES:
+                </li>
+                <li id="from" class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-center
+              ">
+                          FROM:
+                      </li>
+                      <li id="to" class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-center
+              ">
+                          TO:
+          </li>
+        </ul>
+      </div>
+<!--
+      <div class="col-md-12">
         <form>
           <h3>Add a new marker</h3>
           <label for="lat"></label>
@@ -36,10 +92,16 @@
           <label for="lon"></label>
           <input type="text" placeholder="Enter lng">
         </form>
-      </div>
+      </div>--->
     <section>
       <br>
-      <div id="map" style="width:400px;height:400px;" ></div>
+      <div id="map" style="width:800px;height:800px;" ></div>
+      <input
+      id="pac-input"
+      class="controls"
+      type="text"
+      placeholder="Search Box"
+    />
     </section>
     </div>   
   </div> 
@@ -61,7 +123,16 @@
     display: flex;
     justify-content: center;
   }
-
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 400px;
+}
 
 </style>
 
--- ride_location/src/map.js
+++ ride_location/src/map.js
@@ -1,7 +1,161 @@
-//npm install --save axios
-//import axios from "axios";
-//import { response } from "express";
-var axios = require('axios');
+function getLocation(){
+    var returnObject = {};
+    if('geolocation' in navigator){
+        navigator.geolocation.getCurrentPosition(
+        (position) => {
+        returnObject["lat"] = position.coords.latitude;
+        returnObject["lng"] = position.coords.longitude;
+        initMap(returnObject.lat,returnObject.lng);
+        });
+        if(typeof lat ==='undefined' && typeof lng==='undefined'){
+            return returnObject['lat'], returnObject['lng']
+        }
+    }
+}
+
+//var newVal = getLocation();
+//console.log(newVal['lat'])
+/*
+var coords = getLocation();
+if (coords.lat && coords.lng){
+    initMap(coords.lat,coords.lng);
+}
+*/
+//Labels for the markers by clicking on the screen
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex =0;
+//main google maps API
+function initMap(latitude, longitude) {
+    // The user location 
+    const userLocation = { lat: latitude, lng: longitude };
+    //The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+    center: userLocation,
+    zoom: 15,
+    });
+    var mk1 = new google.maps.Marker({position: userLocation,
+    map,
+    });
+    
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, "click", (event) => {
+    addMarker(event.latLng, map);
+    });
+
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    //position in the map 
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+    
+    });
+  
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+    const icon = {
+        url: place.icon,
+        size: new google.maps.Size(81, 81),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+
+  let directionsService = new google.maps.DirectionsService();
+  let directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsRenderer.setMap(map); // Existing map object displays directions
+  // Create route from existing points used for markers
+  const route = {
+      origin: userLocation,
+      destination: markers[0],
+      travelMode: 'DRIVING'
+  }
+
+  directionsService.route(route,
+    function(response, status) { // anonymous function to capture directions
+      if (status !== 'OK') {
+        window.alert('Directions request failed due to ' + status);
+        return;
+      } else {
+        directionsRenderer.setDirections(response); // Add route to the map
+        var directionsData = response.routes[0].legs[0]; // Get data about the mapped route
+        if (!directionsData) {
+          window.alert('Directions request failed');
+          return;
+        }
+        else {
+          document.getElementById('msg').innerHTML += " Driving distance is " + directionsData.distance.text + " (" + directionsData.duration.text + ").";
+        }
+      }
+    });
+}
+
+//function to integate more markers on the map where location is the center lat lng
+function addMarker(location, map){
+    new google.maps.Marker({
+        position:location,//{ lat:latitude, lng:longitude},
+        label: labels[labelIndex++ % labels.length],
+        map:map,
+    });
+}
+
+let autocomplete;
+function initAutocomplete(){
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            types:['establishment'],
+            componentRestrictions:{'country':['us']},
+            fiels:['place_id','geometry', 'name']
+        });
+        autocomplete.addListener('place_changed',onPlaceChanged);
+}
+
+//First try to get the distance between two points
+//var axios = require('axios');
 function distance(origin, destination){
     var mapObj = Maps.newDirectionFinder();
     mapObj.setOrigin(origin);
@@ -17,17 +171,8 @@ function distance(origin, destination){
     Logger.log(minutes);
 };
 
-let autocomplete;
-function initAutocomplete(){
-    autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete'),
-        {
-            types:['establishment'],
-            componentRestrictions:{'country':['us']},
-            fiels:['place_id','geometry', 'name']
-        });
-        autocomplete.addListener('place_changed',onPlaceChanged);
-}
+
+
 function onPlaceChanged(){
     var place = autocomplete.getPlace();
     if(!place.geometry){
@@ -40,31 +185,37 @@ function onPlaceChanged(){
 }
 
 
-//main google maps API
-function initMap(latitude, longitude) {
-    // The location of Uluru
-    let center = { lat: latitude, lng: longitude };
-    //The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-    center: center,
-    zoom: 15,
+function initialize() {
+    var address = (document.getElementById('pac-input'));
+    var autocomplete = new google.maps.places.Autocomplete(address);
+    autocomplete.setTypes(['geocode']);
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+
+    var address = '';
+    if (place.address_components) {
+        address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+    }
+    /*********************************************************************/
+    /* var address contain your autocomplete address *********************/
+    /* place.geometry.location.lat() && place.geometry.location.lat() ****/
+    /* will be used for current address latitude and longitude************/
+    /*********************************************************************/
+    document.getElementById('lat').innerHTML = place.geometry.location.lat();
+    document.getElementById('long').innerHTML = place.geometry.location.lng();
     });
-    new google.maps.Marker({
-    position: center,
-    map,
-  });
 }
 
 
-
-
-//function to integate more markers on the map 
-function marker(){
-    marker = new google.maps.Marker({
-    position: center,
-    map: map,
-    })
-}
+    
+/*
 function showUserLocationOnTheMap(latitude,longitude){
 //creation of the map objects
 let map = new google.maps.Map(document.getElementById("map"), {
@@ -74,7 +225,6 @@ mapTypeId: google.maps.mapTypeId.ROADMAP
     })
     
 }
-
 
 export default {
     data(){
@@ -90,6 +240,8 @@ export default {
                 lng:0 },
         };
     },
+
+
     created(){                   
         // Geolocation
         if (navigator.geolocation) {
@@ -99,7 +251,6 @@ export default {
                 this.myCoordinates.lng=position.coords.longitude;
                 let lat = position.coords.latitude;
                 let lng = position.coords.longitude;
-                initMap(lat,lng);
             });
             (error) => {
                 // console.log('Error', error.message);
@@ -107,72 +258,9 @@ export default {
             }
         } else {
             // console.log('Browser does not support geolocation');
-        }}}
-    
-/*
-
-function 
-
-getAddressFrom(lat,long){
-    axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlong=")
-    + lat 
-    + ","
-    +long
-    +"AIzaSyCSLoEe5amg_DqF7t-XQnFVtSrUFtksZS0")
-    .then(respose=>{
-        if(response.data.error_message){
-
-        }else{
-            console.log(response.data.results[0].formatted_address);
         }
-    })
-    .catch(error=>{
-        console.log(error.message);
-    })
-}
-map = new google.maps.Map(document.getElementById('map'), {
-center: { lat: position.coords.latitude, lng: -74.7520844 },
-zoom: 8
-});
-/*
-var googleMaps = new google.maps.Map(document.getElementById('map'), {
-center: { lat: position.coords.latitude, lng: -74.7520844 },
-zoom: 8
-});
-
-showUserLocationOnTheMap(latitude,longitude){
-//creation of the map objects
-    let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: new google.maps.LatLng(latitude,longitude),
-    mapTypeId: google.maps.mapTypeId.ROADMAP 
-    });
     }
-}
+}; 
 
 
-    mounted(){
-      this.map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-    },
-        
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("map"),mapProp);
-}
-
-        <GmapMap
-         :center="{lat:10,lng:10}">
-        <GmapMarker
-            v-for="(m, index) in markers"
-            :key="index"
-            :position="m.position"
-            @click="center = m.position"
-            /> 
-</GmapMap>     
-*/
+    */
